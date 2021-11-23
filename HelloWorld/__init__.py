8690530a7668308c229c56fded660ba4846dc438
Blake Evans <32441662+BlakeEvans22@users.noreply.github.com>
2021-11-23 17:39:29 -0500

    Update __init__.py

--- HelloWorld/__init__.py
+++ HelloWorld/__init__.py
@@ -19,6 +19,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello {name}!")
     else:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
+            "Please pass a name on the query string or in the request body TEST",
+            status_code=400
         )
